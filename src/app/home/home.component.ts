f4db464a641d596654cf644aa81bbbc1d80eb7f5
zak <mz.soufiani@gmail.com>
2017-05-19 16:57:16 +1000

    Added FA icons

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,19 +9,20 @@ import {NgForm} from "@angular/forms";
 export class HomeComponent implements OnInit {
 
   public  values = [];
-
+  showImage: boolean = false;
 
   onSubmit(f: NgForm){
     console.log(f.value);
     console.log(f.valid);
     this.values = f.value;
+    this.showImage = true;
     console.log(this.values);
   }
 
   clearValues(){
     this.values = [];
     console.log(this.values);
-
+    this.showImage = false;
   }
 
   constructor() { }
